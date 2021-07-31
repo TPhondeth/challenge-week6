c6c74171551321176847667958c37fb2255a51fe
Tony Phondeth <phondeth@gmail.com>
2021-07-31 12:45:41 -0400

    modified js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,54 @@
 // Declaration of Variables
-let name = document.querySelector('.name');
-let temp = document.querySelector('.temp');
-let wind = document.querySelector('.wind');
-let humidity = document.querySelector('.humidity');
-let btnPrimary = document.querySelector('.btn-primary');
+let nameEl = document.querySelector('.name');
+let tempEl = document.querySelector('.temp');
+let windEl = document.querySelector('.wind');
+let humidityEL = document.querySelector('.humidity');
+let searchBtnEl = document.querySelector('.btn-primary');
 
 // API Key
 const apiKey = '8cb2238a37cf037e6cb64bd1b66f4f0a';
 
 function myWeather() {
+    // Fetch request to Open Weather Map API
     let cityName = document.querySelector('#cityname').value;
-    
     fetch(
         'http://api.openweathermap.org/data/2.5/weather?q=' +
         cityName +
-        '&appid=8cb2238a37cf037e6cb64bd1b66f4f0a&units=metric'
+        '&appid=' +
+        apiKey
     )
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
+        console.log(data);
         let nameValue = data['name'];
         let tempValue = data['main']['temp'];
         let windValue = data['wind']['speed'];
         let humidityValue = data['main']['humidity'];
+        let currentDate = new Date().toLocaleDateString()
         
-        
+        // Display current date and weather conditions
+        nameEl.innerHTML = nameValue + " " + currentDate; 
+        tempEl.innerHTML = "Temp: " + tempValue + " " + "&#x2103;";
+        windEl.innerHTML = "Wind: " + windValue + " " + "MPH";
+        humidityEL.innerHTML = "Humidity: " + humidityValue + " " + "&#x25;" ;
+    })
+    .catch(function(err) {
+        alert('Please search a valid city!');
+    })
 
-        name.innerHTML = nameValue;
-        temp.innerHTML = "Temp: " + tempValue + " " + "&#x2103;";
-        wind.innerHTML = "Wind: " + windValue + " " + "MPH";
-        humidity.innerHTML = "Humidity: " + humidityValue + " " + "&#x25;" ;
+    // Five Day Forecast
+    let forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + apiKey;
+    .then(function(response) {
+        console.log(forecastURL);
+
+        const forecastEl = document.querySelectorAll('forecast');
     })
 }
-    btnPrimary.addEventListener('click', myWeather)
+
+    
+    searchBtnEl.addEventListener('click', myWeather)
 
 
     
