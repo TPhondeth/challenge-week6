f2199452538b4e2dcaf303ba6e3b978acb210723
Tony Phondeth <phondeth@gmail.com>
2021-08-01 19:46:06 -0400

    5 day forecast values

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ let tempEl = document.querySelector('.temp');
 let windEl = document.querySelector('.wind');
 let humidityEL = document.querySelector('.humidity');
 let uvEl = document.querySelector('.uv');
+let uvIndexEl = document.querySelector('.uvIndex')
 let searchBtnEl = document.querySelector('.btn-primary');
 
 // API Key
@@ -38,18 +39,66 @@ function myWeather() {
         tempEl.innerHTML = "Temp: " + data.main.temp + " " + "&#x2103;";
         windEl.innerHTML = "Wind: " + data.wind.speed  + " " + "MPH";
         humidityEL.innerHTML = "Humidity: " + data.main.humidity + " " + "&#x25;" ;
+        
+        // Fetch request to retrieve UV index
+        let lat = data.coord.lat;
+        let lon = data.coord.lon;
+        return fetch(
+            'https://api.openweathermap.org/data/2.5/uvi/forecast?lat=' +
+            lat + '&lon=' + lon +
+            '&appid=' + apiKey + '&cnt=1'
+        )
     })
-    // Query to retrieve UV index
-    return fetch(
-                'https://api.openweathermap.org/data/2.5/uvi/forecast?lat=' + lat + '&lon=' + 
-                lon + '&appid=' + apiKey + '&cnt=1'
-    )
+    .then(function(response) {
+        return response.json();
+    })
     .then(function(data) {
         console.log(data);
-        
-        // uvEl.innerHTML = "UV Index: " + data.
+        uvIndexEl.innerHTML = data[0].value
+        uvEl.innerHTML = "UV Index: ";
+        uvEl.appendChild(uvIndexEl);
 
+        // Fetch request for 5-Day Forecast
+        return fetch(
+            'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=metric' + "&appid=" + apiKey
+        )
     })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        const forecastEl = document.querySelectorAll('.forecast');
+        // Parse data to display 5-Day Forecast
+        for (i = 0; i < forecastEl.length; i++) {
+            forecastEl[i].innerHTML= "";
+            const forecastIndex = i*8 + 5;
+            const forecastDateEl = document.createElement('p');
+            const forecastIconEl = document.createElement('img');
+            const forecastTempEl = document.createElement('p');
+            const forecastWindEl = document.createElement('p');
+            const forecastHumidityEl = document.createElement('p');
+            const forecastDate = new Date(data.list[forecastIndex].dt * 1000);
+            const forecastDay = forecastDate.getDate();
+            const forecastMonth = forecastDate.getMonth() + 1;
+            const forecastYear = forecastDate.getUTCFullYear();
+
+            forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+            forecastEl[i].append(forecastDateEl);
+            forecastTempEl.innerHTML = "Temp: " + (data.list[forecastIndex].main.temp) + " " + "&#x2103;";
+            forecastEl[i].append(forecastTempEl);
+            forecastWindEl.innerHTML = "Wind: " + (data.list[forecastIndex].wind.speed) + " " +  "MPH";
+            forecastEl[i].append(forecastWindEl);
+            forecastHumidityEl.innerHTML = "Humidity: " + (data.list[forecastIndex].main.humidity) + " " +  "&#x25;";
+            forecastEl[i].append(forecastHumidityEl);
+            forecastIconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[forecastIndex].weather[0].icon + "@2x.png");
+            forecastIconEl.setAttribute("alt", data.list[forecastIndex].weather[0].description);
+            forecastEl[i].append(forecastIconEl);
+
+            
+        }
+    })
+
 }
     searchBtnEl.addEventListener('click', myWeather)
 
